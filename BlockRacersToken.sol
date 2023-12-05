82c8e2e08250371421179ab760338ebb544b00de
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2023-12-05 21:20:39 +0800

    URI and type

--- BlockRacersToken.sol
+++ BlockRacersToken.sol
@@ -20,12 +20,51 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract BlockRacersToken is ERC20, ReentrancyGuard {
     constructor() ERC20("BlockRacersToken", "RACE") {}
 
+    ///@dev Mappings
+    /// @dev Wallet that auth signatures come from
+    address authWallet = 0x0d9566FcE2513cBD388DCD7749a873900033401C;
+    /// @dev Nonce to stop cheaters
+    mapping(address => uint256) public nonce;
+
     /// @dev Used to mint tokens to an address
     /// @param _to The address to mint tokens to
     /// @param _amount The amount of tokens to mint to the address
     /// @return true if  mint is successful
-    function mint(address _to, uint256 _amount) public nonReentrant() returns (bool) {
+    function mint(address _to, uint256 _amount, bytes memory _sig) public nonReentrant() returns (bool) {
+        bytes32 messageHash = getMessageHash(abi.encodePacked(nonce[msg.sender], msg.sender, _amount));
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+        require(recover(ethSignedMessageHash, _sig) == authWallet, "Sig not made by auth");
         _mint(_to, _amount);
+        nonce[msg.sender]++;
         return true;
+    }
+
+    /// @dev Used for authentication to check if values came from inside the Block Racers game following solidity standards
+    function VerifySig(address _signer, bytes memory _message, bytes memory _sig) external pure returns (bool) {
+        bytes32 messageHash = getMessageHash(_message);
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+        return recover(ethSignedMessageHash, _sig) == _signer;
+    }
+
+    function getMessageHash(bytes memory _message) internal pure returns (bytes32) {
+        return keccak256(_message);
+    }
+
+    function getEthSignedMessageHash(bytes32 _messageHash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32",_messageHash));
+    }
+
+    function recover(bytes32 _ethSignedMessageHash, bytes memory _sig) internal pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = _split(_sig);
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function _split (bytes memory _sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(_sig.length == 65, "Invalid signature length");
+        assembly {
+            r := mload(add(_sig, 32))
+            s := mload(add(_sig, 64))
+            v := byte(0, mload(add(_sig, 96)))
+        }
     }
 }
