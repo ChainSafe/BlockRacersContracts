2df8a0caa714198801abb3a8260c2b02a8d3dab2
RyRy79261 <ryanjnoble@gmail.com>
2023-12-30 21:32:34 +0200

    Blacklist complete

--- test/Blacklist.test.ts
+++ test/Blacklist.test.ts
@@ -1,24 +1,78 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { addToBlacklist, addToBlacklistWithErrors, addToBlacklistWithEvents, deployBlacklistFixture, isBlackListed, removeFromBlackList, removeFromBlackListWithErrors, removeFromBlackListWithEvents } from "./contractFunctions/Blacklist.contract";
+import { getAccounts } from "./contractFunctions/generalFunctions";
+import { assert } from "chai";
+
 describe("Blacklist", function () {
     describe("deployment", () => {
-        it("deploys as expected")  
+        it("deploys as expected", async () => {
+            const { admin } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+            
+            const owner = await blacklistContract.owner()
+            assert(owner == admin.address, `Contract not set up correctly. Owner: ${owner} | Expected: ${admin.address}`)
+        })  
     })
     describe("read", function () {
-        it("isBlackListed")
+        it("isBlackListed", async () => {
+            const { admin, player1 } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+
+            await isBlackListed(blacklistContract, player1.address, false)
+            await addToBlacklist(blacklistContract, admin, player1.address)
+            await isBlackListed(blacklistContract, player1.address, true)
+
+        })
     });
 
     describe("write", function () {
-        it("addToBlackList")
-        it("removeFromBlackList")
+        it("addToBlackList", async () => {
+            const { admin, player1 } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+
+            await addToBlacklist(blacklistContract, admin, player1.address)
+        })
+        it("removeFromBlackList", async () => {
+            const { admin, player1 } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+
+            await addToBlacklist(blacklistContract, admin, player1.address)
+
+            await isBlackListed(blacklistContract, player1.address, true)
+            await removeFromBlackList(blacklistContract, admin, player1.address)
+            await isBlackListed(blacklistContract, player1.address, false)
+        })
     });
 
     describe("events", function () {
-        it("AddedToBlacklist")
-        it("RemovedFromBlacklisted")
+        it("AddedToBlacklist", async () => {
+            const { admin, player1 } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+
+            await addToBlacklistWithEvents(blacklistContract, admin, player1.address, "AddedToBlacklist", [player1.address])
+        })
+        it("RemovedFromBlacklisted", async () => {
+            const { admin, player1 } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+            await addToBlacklist(blacklistContract, admin, player1.address)
+
+            await removeFromBlackListWithEvents(blacklistContract, admin, player1.address, "RemovedFromBlacklisted", [player1.address])
+        })
     })
 
     describe("errors", function () {
         it("AccountBlacklisted")
-        it("AccountAlreadyBlacklisted")
-        it("AccountNotBlacklisted")
+        it("AccountAlreadyBlacklisted", async () => {
+            const { admin, player1 } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+
+            await addToBlacklistWithErrors(blacklistContract, admin, player1.address, `AccountAlreadyBlacklisted`, [player1.address])
+        })
+        it("AccountNotBlacklisted", async () => {
+            const { admin, player1 } = await getAccounts()
+            const blacklistContract = await loadFixture(deployBlacklistFixture)
+
+            await removeFromBlackListWithErrors(blacklistContract, admin, player1.address, "AccountNotBlacklisted", [player1.address])
+        })
     })
 });
