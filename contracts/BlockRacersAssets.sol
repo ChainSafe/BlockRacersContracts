8dd1d048c9d8e35a4cf12144f2c6354285327560
RyRy79261 <ryanjnoble@gmail.com>
2024-01-15 11:54:59 +0200

    Most solhint done

--- contracts/BlockRacersAssets.sol
+++ contracts/BlockRacersAssets.sol
@@ -1,49 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
-
-// $$$$$$$\  $$\       $$$$$$\   $$$$$$\  $$\   $$\       $$$$$$$\   $$$$$$\   $$$$$$\  $$$$$$$$\ $$$$$$$\   $$$$$$\  
-// $$  __$$\ $$ |     $$  __$$\ $$  __$$\ $$ | $$  |      $$  __$$\ $$  __$$\ $$  __$$\ $$  _____|$$  __$$\ $$  __$$\ 
-// $$ |  $$ |$$ |     $$ /  $$ |$$ /  \__|$$ |$$  /       $$ |  $$ |$$ /  $$ |$$ /  \__|$$ |      $$ |  $$ |$$ /  \__|
-// $$$$$$$\ |$$ |     $$ |  $$ |$$ |      $$$$$  /        $$$$$$$  |$$$$$$$$ |$$ |      $$$$$\    $$$$$$$  |\$$$$$$\  
-// $$  __$$\ $$ |     $$ |  $$ |$$ |      $$  $$<         $$  __$$< $$  __$$ |$$ |      $$  __|   $$  __$$<  \____$$\ 
-// $$ |  $$ |$$ |     $$ |  $$ |$$ |  $$\ $$ |\$$\        $$ |  $$ |$$ |  $$ |$$ |  $$\ $$ |      $$ |  $$ |$$\   $$ |
-// $$$$$$$  |$$$$$$$$\ $$$$$$  |\$$$$$$  |$$ | \$$\       $$ |  $$ |$$ |  $$ |\$$$$$$  |$$$$$$$$\ $$ |  $$ |\$$$$$$  |
-// \_______/ \________|\______/  \______/ \__|  \__|      \__|  \__|\__|  \__| \______/ \________|\__|  \__| \______/ 
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {
+    ERC1155URIStorage
+} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import {
+    ReentrancyGuard
+} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {
+    ERC2771Context,
+    Context
+} from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
 /// @title Block Racers ERC1155 contract
 /// @author RyRy79261
-/// @notice This contract facilitates NFT asset management in Block Racers at https://github.com/Chainsafe/BlockRacers
-contract BlockRacersAssets is ERC2771Context, ERC1155, ERC1155URIStorage, AccessControl, ReentrancyGuard {
+/// @notice This contract facilitates NFT asset management in Block Racers
+contract BlockRacersAssets is
+    ERC2771Context,
+    ERC1155,
+    ERC1155URIStorage,
+    AccessControl,
+    ReentrancyGuard
+{
     bytes32 public constant BLOCK_RACERS = keccak256("BLOCK_RACERS");
-    
+
     error UriArrayLengthInvalid();
     error NotAuthorizedGameContract();
 
-    modifier onlyBlockracers {
-        if(!hasRole(BLOCK_RACERS, _msgSender()))
+    modifier onlyBlockracers() {
+        if (!hasRole(BLOCK_RACERS, _msgSender()))
             revert NotAuthorizedGameContract();
-        
+
         _;
     }
 
-    /// @dev Constructor sets token to be used and nft info, input the RACE token address here on deployment
+    /// @dev Constructor sets token to be used and nft info
     /// @param trustedForwarder ERC2771 relayer address
     /// @param baseUri_ URI base string
     /// @param _admin Admin address for managing approved minters
     constructor(
         address trustedForwarder,
-        string memory baseUri_, 
+        string memory baseUri_,
         address _admin
     ) ERC2771Context(trustedForwarder) ERC1155(baseUri_) {
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
-        // Default is 0x00 so the default init value of Admin role would be 0x00 so this might be redundant
-        _setRoleAdmin(BLOCK_RACERS, DEFAULT_ADMIN_ROLE); 
+        // Default is 0x00 so the default init value of Admin role
+        // would be 0x00 so this might be redundant
+        _setRoleAdmin(BLOCK_RACERS, DEFAULT_ADMIN_ROLE);
     }
 
     /// @dev Minting functions
@@ -52,10 +57,17 @@ contract BlockRacersAssets is ERC2771Context, ERC1155, ERC1155URIStorage, Access
     /// @param id The ID of the token
     /// @param value The amount of token being sent
     /// @return true if successful
-    function mint(address to, uint256 id, uint256 value, string memory newUri) external onlyBlockracers() returns(bool) {
-       _mint(to, id, value, new bytes(0));
-       ERC1155URIStorage._setURI(id, newUri);
-       return true;
+    function mint(
+        address to,
+        uint256 id,
+        uint256 value,
+        string memory newUri
+    ) external onlyBlockracers returns (bool) {
+        _mint(to, id, value, new bytes(0));
+
+        ERC1155URIStorage._setURI(id, newUri);
+
+        return true;
     }
 
     /// Batch minting function
@@ -63,41 +75,59 @@ contract BlockRacersAssets is ERC2771Context, ERC1155, ERC1155URIStorage, Access
     /// @param ids ID list
     /// @param values Quantity list
     /// @param uriList URI list
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, string[] memory uriList) external onlyBlockracers() returns(bool) {
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values,
+        string[] memory uriList
+    ) external onlyBlockracers returns (bool) {
         uint256 nftCount = uriList.length;
-        if(ids.length != nftCount)
-            revert UriArrayLengthInvalid();
-            
+        if (ids.length != nftCount) revert UriArrayLengthInvalid();
+
         _mintBatch(to, ids, values, new bytes(0));
 
-        for(uint256 i = 0; i < nftCount;) {
+        for (uint256 i = 0; i < nftCount; ) {
             ERC1155URIStorage._setURI(ids[i], uriList[i]);
-            unchecked{
+            unchecked {
                 i++;
             }
         }
         return true;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC1155, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function uri(uint256 tokenId) public view override(ERC1155URIStorage, ERC1155) returns (string memory) {
+    function uri(
+        uint256 tokenId
+    ) public view override(ERC1155URIStorage, ERC1155) returns (string memory) {
         return ERC1155URIStorage.uri(tokenId);
     }
 
     /**
      * @dev Override required as inheritance was indeterminant for which function to use
      */
-    function _msgSender() internal view override(ERC2771Context, Context) returns (address sender) {
+    function _msgSender()
+        internal
+        view
+        override(ERC2771Context, Context)
+        returns (address sender)
+    {
         return ERC2771Context._msgSender();
     }
 
     /**
      * @dev Override required as inheritance was indeterminant for which function to use
      */
-    function _msgData() internal view override(ERC2771Context, Context) returns (bytes calldata) {
+    function _msgData()
+        internal
+        view
+        override(ERC2771Context, Context)
+        returns (bytes calldata)
+    {
         return ERC2771Context._msgData();
     }
 }
--- contracts/utils/Blacklist.sol
+++ contracts/utils/Blacklist.sol
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
-
-// $$$$$$$\  $$\       $$$$$$\   $$$$$$\  $$\   $$\       $$$$$$$\   $$$$$$\   $$$$$$\  $$$$$$$$\ $$$$$$$\   $$$$$$\  
-// $$  __$$\ $$ |     $$  __$$\ $$  __$$\ $$ | $$  |      $$  __$$\ $$  __$$\ $$  __$$\ $$  _____|$$  __$$\ $$  __$$\ 
-// $$ |  $$ |$$ |     $$ /  $$ |$$ /  \__|$$ |$$  /       $$ |  $$ |$$ /  $$ |$$ /  \__|$$ |      $$ |  $$ |$$ /  \__|
-// $$$$$$$\ |$$ |     $$ |  $$ |$$ |      $$$$$  /        $$$$$$$  |$$$$$$$$ |$$ |      $$$$$\    $$$$$$$  |\$$$$$$\  
-// $$  __$$\ $$ |     $$ |  $$ |$$ |      $$  $$<         $$  __$$< $$  __$$ |$$ |      $$  __|   $$  __$$<  \____$$\ 
-// $$ |  $$ |$$ |     $$ |  $$ |$$ |  $$\ $$ |\$$\        $$ |  $$ |$$ |  $$ |$$ |  $$\ $$ |      $$ |  $$ |$$\   $$ |
-// $$$$$$$  |$$$$$$$$\ $$$$$$  |\$$$$$$  |$$ | \$$\       $$ |  $$ |$$ |  $$ |\$$$$$$  |$$$$$$$$\ $$ |  $$ |\$$$$$$  |
-// \_______/ \________|\______/  \______/ \__|  \__|      \__|  \__|\__|  \__| \______/ \________|\__|  \__| \______/ 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {
+    ERC2771Context,
+    Context
+} from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
 /// @title Block Racers blacklisting contract
 /// @author RyRy79261
-/// @notice This contract allows admins to block specific accounts used in the game at https://github.com/Chainsafe/BlockRacers
+/// @notice This contract allows admins to block specific
+/// accounts used in the game at https://github.com/Chainsafe/BlockRacers
 contract Blacklist is ERC2771Context, Ownable {
-    mapping(address => bool) private blacklisted;
+    mapping(address => bool) private _blacklisted;
     event AddedToBlacklist(address indexed wallet);
     event RemovedFromBlacklisted(address indexed wallet);
 
@@ -26,8 +21,7 @@ contract Blacklist is ERC2771Context, Ownable {
     error AccountNotBlacklisted(address wallet);
 
     modifier isNotBlacklisted(address account) {
-        if (blacklisted[account])
-            revert AccountBlacklisted(account);
+        if (_blacklisted[account]) revert AccountBlacklisted(account);
 
         _;
     }
@@ -35,39 +29,49 @@ contract Blacklist is ERC2771Context, Ownable {
     constructor(
         address admin_,
         address trustedForwarder
-        ) Ownable(admin_)ERC2771Context(trustedForwarder) {}
+    ) Ownable(admin_) ERC2771Context(trustedForwarder) {}
 
-    function addToBlackList(address account) external onlyOwner() {
-        if (blacklisted[account])
-            revert AccountAlreadyBlacklisted(account);
+    function addToBlackList(address account) external onlyOwner {
+        if (_blacklisted[account]) revert AccountAlreadyBlacklisted(account);
 
-        blacklisted[account] = true;
+        _blacklisted[account] = true;
         emit AddedToBlacklist(account);
     }
 
-    function removeFromBlackList(address account) external onlyOwner() {
-         if (!blacklisted[account])
-            revert AccountNotBlacklisted(account);
+    function removeFromBlackList(address account) external onlyOwner {
+        if (!_blacklisted[account]) revert AccountNotBlacklisted(account);
 
-        blacklisted[account] = false;
+        _blacklisted[account] = false;
         emit RemovedFromBlacklisted(account);
     }
 
-    function isBlackListed(address account) public view returns(bool) {
-        return blacklisted[account];
+    function isBlackListed(address account) public view returns (bool) {
+        return _blacklisted[account];
     }
 
     /**
      * @dev Override required as inheritance was indeterminant for which function to use
      */
-    function _msgSender() internal view virtual override(ERC2771Context, Context) returns (address sender) {
+    function _msgSender()
+        internal
+        view
+        virtual
+        override(ERC2771Context, Context)
+        returns (address sender)
+    {
         return ERC2771Context._msgSender();
     }
 
     /**
      * @dev Override required as inheritance was indeterminant for which function to use
      */
-    function _msgData() internal view virtual override(ERC2771Context, Context) returns (bytes calldata) {
+    function _msgData()
+        internal
+        view
+        virtual
+        override(ERC2771Context, Context)
+        returns (bytes calldata)
+    {
         return ERC2771Context._msgData();
     }
 }
